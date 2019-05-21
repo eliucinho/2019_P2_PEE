204e9ae413d2521cdfad59e60e1f785ee50d1f6a
Eliud Varela <adelman.varela@gmail.com>
2019-05-20 22:35:26 -0600

    Clase de intro a Netbeans

---
+++ PEE2019_P2_IntroNEtbeans/src/uth/programa.java
@@ -0,0 +1,46 @@
+// indico el paquete al que pertenece el programa
+package uth;
+
+/*
+COmentario
+a
+e
+i
+o
+u
+*/
+
+public class programa {
+ 
+    //METODO PRINCIPAL: es el punto de entrada en la aplicacion
+    public static void main(String[] args) {
+        System.out.println("Hola UTH");
+        System.out.println("");
+        
+        //Atajo sout y luego tabular
+        System.out.println("");
+        System.out.println("Hola Honduras");
+        
+        System.out.println("================================");
+        System.out.println("No.  ll    NOmbre   11 apellido");
+        System.out.println("================================");
+        
+        //Como definir variables
+        //Texto
+        String nombreVariableTexto;
+        String nombreVariableTextoConValor="Hola soy un valor";
+        
+        //Solo una letra
+        char nombreVariableSoloUnaLetra;
+        char nombreVariableSoloUnaLetraConValor='c';
+        
+        //imprimiendo un String
+        System.out.println(nombreVariableTextoConValor);
+        
+        //imprimiendo un char
+        System.out.println(nombreVariableSoloUnaLetraConValor);
+        
+        
+    }
+    
+}
